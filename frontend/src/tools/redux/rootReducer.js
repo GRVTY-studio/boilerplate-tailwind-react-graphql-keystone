64bb5743c9b88f020035be66041ff1fa82e7533a
Sanket Agarwal <sanket.agarwal@postmates.com>
2021-04-15 17:21:11 -0700

    Reduce redux

--- frontend/src/tools/redux/rootReducer.js
+++ frontend/src/tools/redux/rootReducer.js
@@ -14,12 +14,4 @@ import taskConfirmationReducer from './slices/taskConfirmationSlice';
 export default combineReducers({
     error: errorReducer,
     auth: authReducer,
-    quotes: quoteReducer,
-    company: companyReducer,
-    integrations: integrationReducer,
-    industries: industriesReducer,
-    employees: employeesReducer,
-    departments: departmentsReducer,
-    yearlyIncome: yearlyIncomeReducer,
-    taskConfirmation: taskConfirmationReducer,
 });
