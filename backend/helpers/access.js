585de9dbf7013fb89ed6a07c207eb503c5f4e5de
Gabi Dobocan <gabi@telepat.io>
2021-04-28 03:13:17 +0300

    Cleanup access helpers

--- backend/helpers/access.js
+++ backend/helpers/access.js
@@ -18,40 +18,14 @@ const ownsItemViaUser = async ({ authentication: { item: user } }) => {
     return { user: { id: user.id } };
 };
 
-const ownsItemViaIntegration = async ({ authentication: { item: user } }) => {
-    if (!user) {
-        return false;
-    }
-
-    return { integration: { user: { id: user.id } } };
-};
-
-const ownsItemViaEmployee = async ({ authentication: { item: user } }) => {
-    if (!user) {
-        return false;
-    }
-
-    return { employee: { user: { id: user.id } } };
-};
-
 const isAdminOrUser = (auth) => isAdmin(auth) || isUser(auth);
 
 const isAdminOrOwner = async (auth) => isAdmin(auth) || ownsItemViaUser(auth);
-
-const isAdminOrIntegrationOwner = async (auth) =>
-    isAdmin(auth) || ownsItemViaIntegration(auth);
-
-const isAdminOrEmployeeOwner = async (auth) =>
-    isAdmin(auth) || ownsItemViaEmployee(auth);
 
 module.exports = {
     isUser,
     isAdmin,
     ownsItemViaUser,
-    ownsItemViaIntegration,
-    ownsItemViaEmployee,
     isAdminOrOwner,
-    isAdminOrIntegrationOwner,
-    isAdminOrEmployeeOwner,
     isAdminOrUser,
 };
